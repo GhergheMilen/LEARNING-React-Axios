995b80b96e5dd2933ce8e9fa883cd4cb8d98a6a0
GhMilen <gherghemilen@gmail.com>
2025-10-23 17:14:25 +0300

    Fixed more prop drilling with component composition

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -58,11 +59,16 @@ export default function App() {
         <Search></Search>
         <NumResults movies={movies}></NumResults>
       </Navbar>
+
       <Main>
-        <ListBox>
+        <Box>
           <MovieList movies={movies}></MovieList>
-        </ListBox>
-        <WatchedBox></WatchedBox>
+        </Box>
+
+        <Box>
+          <WatchedSummary watched={watched}></WatchedSummary>
+          <WatchedMovieList watched={watched}></WatchedMovieList>
+        </Box>
       </Main>
     </>
   );
@@ -109,14 +115,15 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function ListBox({ children }) {
-  const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="box">
-      <button className="btn-toggle" onClick={() => setIsOpen1((open) => !open)}>
-        {isOpen1 ? "–" : "+"}
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
       </button>
-      {isOpen1 && children}
+
+      {isOpen && children}
     </div>
   );
 }
@@ -143,24 +150,6 @@ function Movie({ movie }) {
         </p>
       </div>
     </li>
-  );
-}
-function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  return (
-    <div className="box">
-      <button className="btn-toggle" onClick={() => setIsOpen2((open) => !open)}>
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched}></WatchedSummary>
-          <WatchedMovieList watched={watched}></WatchedMovieList>
-        </>
-      )}
-    </div>
   );
 }
 
