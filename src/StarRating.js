82cae7ce5e8c9e9d35bee58bfd8ac3e981009c39
GhMilen <gherghemilen@gmail.com>
2025-10-26 19:28:54 +0200

    Learned about hover effect with onMouseEnter and onMouseLeave.

--- src/StarRating.js
+++ src/StarRating.js
@@ -25,6 +25,7 @@ const starStyle = {
 // setting up a default maxRating = 5
 export default function StarRating({ maxRating = 10 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -35,18 +36,30 @@ export default function StarRating({ maxRating = 10 }) {
       <div style={starContainerStar}>
         {Array.from({ length: maxRating }, (_, i) => (
           <span>
-            <Star key={i} onRate={() => handleRating(i + 1)} full={rating >= i + 1} />
+            <Star
+              key={i}
+              onRate={() => handleRating(i + 1)}
+              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
+            />
           </span>
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}> {tempRating || rating || ""} </p>
     </div>
   );
 }
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
