99d6d7a6f47ea79c80842d16be37f8543972e869
GhMilen <gherghemilen@gmail.com>
2025-12-06 22:57:45 +0200

    Learned about useRef, lazy evaluation, updating state with a callback function.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import StarRating from "./StarRating";
 
@@ -281,6 +281,22 @@ function Logo() {
   );
 }
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputEl.current) {
+        return;
+      }
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -288,6 +304,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
